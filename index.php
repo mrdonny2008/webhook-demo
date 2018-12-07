64234599dbde68fb1b0e376354f42b00c558b1c1
DONNY FERNANDO <mr.donny2008@gmail.com>
2018-12-07 21:32:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,41 @@
-<html>
- <head>
-  <title>PHP Test123</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p> jagoan saya'; ?> 
- </body>
-</html>
+<?php
+
+$method = $_SERVER('REQUEST_METHOD');
+
+//Prosess only when methode is POST 
+if($method == 'POST'){
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+
+    $text = $json->result->parameters->text;
+
+    switch ($text){
+        case 'hi':
+            $speech = "Hi, Nice to meet you";
+            break;
+        
+        case 'bye':
+            $speech = "Bye, good night";
+            break;
+        
+        case 'anything'
+            $speech = "Yes, you can type anything here";
+            break;    
+        
+        default:
+            $speech = "Sorry, I didnt get that. Please ask me something else";
+            break;
+    }
+
+    $response = new\stdClass();
+    $response->speech ="";
+    $response->displayText ="";
+    $response->source ="webhook";
+    echo json_encode($response) 
+}
+else
+{
+    echo "Method not allowed";
+}
+
+?>
